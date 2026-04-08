dee862d48b0da52276079ef9c5a1f035fccf11dc
Haride PC <haridevkrishna@gmail.com>
2026-04-08 15:52:01 +0530

    Added the objective of the program

--- bankaccount.py
+++ bankaccount.py
@@ -1,6 +1,32 @@
 accNo = 10020100
 accCount = 0
 
+# # Bank app using dictionary
+# # Main menu
+# # 1.account create
+# # 2.transaction
+# # 3.all accounts 
+# # 4.exit
+# # Note. Account no should be automaticaly increment which must use as global variable start with 10020100
+# # 1.create account
+# # Read username and amount to open an account
+# # Check the amount is multiple of 100 and must be greater than 1000 then save to dictionary using account no as key
+# 2.transaction
+# Read account no of exist then
+# Show another menu
+# 1.deposit
+# Amount must be multiple of 100
+# 2.withdraw
+# Amount must be multiple of 100 and minimum 1000 must be kept
+# 3.show
+# 4.exit
+
+# Else
+# Show account number is not exist
+# 3.show all account
+# Show all entry in account dictionary
+
+
 def main():
     i = int(
         input(
@@ -9,7 +35,7 @@ def main():
                   2. Transaction\n
                   3. All Accounts\n
                   4. Exit\n"""
-                )
+            )
     )
     match i:
         case 1:
@@ -31,7 +57,7 @@ def createaccount():
     
 
 def Transaction():
-    
+    i = int(input(""))
 
 def account():
 
