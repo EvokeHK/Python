c36155adf4190ae7a8400f0eb4389a6d85641c4b
Haridev <haridevkrishna@gmail.com>
2026-04-08 17:41:38 +0530

    Initial Draft

--- bankaccount.py
+++ bankaccount.py
@@ -52,10 +52,11 @@ def main():
 def createaccount():
     global accNo
     global accDict
+    username = input("Enter the User Name ")
     amount = int(input("Enter the initial Amount "))
     while amount % 100 != 0 and amount > 1000:
         amount = "The initial amount should be multiple of 100 and more than 1000\n Enter the amount again"
-    accDict.update({accNo: amount})
+    accDict.update({accNo: {"Username": username, "Amount": amount}})
     print("Account ", accNo, " Created")
     accNo += 1
     y = input("Do you want to go back to Main Menu (y/n)")
@@ -67,9 +68,50 @@ def createaccount():
 
 
 def Transaction():
+    global accDict, accNo
     acc = int(input("Enter the account Number \n"))
     if acc in accDict.keys():
-        x = int(input("Enter the amount to withdraw"))
+        x = int(
+            input(
+                """MENU \n
+                      1. Withdraw\n
+                      2. Deposit"""
+            )
+        )
+        match x:
+            case 1:
+                z = int(input("Enter the amount to be withdrawn "))
+                if ((accDict[acc]["Amount"] - z) > 1000) and (z % 100 == 0):
+                    accDict[acc]["Amount"] -= z
+                    print("Amount Successfully Withdrawn \n")
+                    y = input("Do you want to go back to Main Menu (y/n)")
+                    match y:
+                        case "y":
+                            main()
+                        case "n":
+                            exit()
+                else:
+                    print(
+                        "Withdrawal amount should be multiple of 100 and final balance should be more than 1000 .....\nreturning to Main Menu"
+                    )
+                    main()
+            case 2:
+                z = int(input("Enter the amount to be deposited "))
+                if z % 100 == 0:
+                    accDict[acc]["Amount"] -= z
+                    print("Amount Deposited\n")
+                    y = input("Do you want to go back to Main Menu (y/n)")
+                    match y:
+                        case "y":
+                            main()
+                        case "n":
+                            exit()
+                else:
+                    print("Deposit amount should be multiple of 100")
+                    main()
+    else:
+        print("Account Not found .... Returning to Main Menu")
+        main()
 
 
 def account():
